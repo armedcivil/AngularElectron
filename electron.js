809cbee6d4c398cbfaaa65af172cecd93ba81870
Mac User <laser_yellow@cnc.jp>
2017-12-02 03:42:05 +0900

    パッケージ化まで途中

--- electron.js
+++ electron.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var electron = require('electron');
+var Path = require('path');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 
@@ -14,10 +15,17 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-
     // ブラウザ(Chromium)の起動, 初期画面のロード
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    var indexPath = __dirname + '/dist/index.html';
+
+    // 環境によって読み込むパスを変える
+    var indexPath = __dirname;
+    if(process.argv[2] === 'debug'){
+        indexPath = Path.join(indexPath, 'dist');
+    }
+    indexPath = Path.join(indexPath, 'index.html');
+
+    // 読み込む
     mainWindow.loadURL('file://' + indexPath);
     mainWindow.on('closed', function() {
         mainWindow = null;
